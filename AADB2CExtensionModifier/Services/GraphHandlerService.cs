3ee5640365179facc22e805a0fc678f03e4740f7
Timothy Winsley <t-winsley@protonmail.com>
2024-04-10 10:23:08 -0700

    Scaffold out more Graph service methods

--- AADB2CExtensionModifier/Services/GraphHandlerService.cs
+++ AADB2CExtensionModifier/Services/GraphHandlerService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿using Microsoft.Graph.Models;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -10,6 +11,7 @@ namespace AADB2CExtensionModifier.Services
 {
     internal class GraphHandlerService
     {
+        // This method is used to get the B2C extension application id and parse it for use in querying the extension attributes.
         public String GetB2cExtensionAppId(string tenantId)
         {
             string appId = string.Empty;
@@ -17,11 +19,20 @@ namespace AADB2CExtensionModifier.Services
             return appId;
         }
 
+        // This method gets the B2C extension attributes for the user.
         public List<string> GetUserExtensionAttributes(string userIdentifier, string tenantId)
         {
             List<string> extensionAttributes = new List<string>();
             // TODO : Implement this method
             return extensionAttributes;
+        }
+
+        // This method gets the user's graph user object. It should take an email as input and return the user object.
+        public User GetGraphUser(string email, string tenantId)
+        {
+            User user = new User();
+            // TODO : Implement this method
+            return user;
         }
     }
 }
