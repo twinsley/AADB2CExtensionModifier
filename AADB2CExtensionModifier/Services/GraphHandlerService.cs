42ec5c26a2eda67c7dd60f6fc31d4d6d513fd565
Timothy Winsley <t-winsley@protonmail.com>
2024-04-10 10:00:20 -0700

    Commit initial files

---
+++ AADB2CExtensionModifier/Services/GraphHandlerService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AADB2CExtensionModifier.Services
+{
+    internal class GraphHandlerService
+    {
+        public String GetB2cExtensionAppId(string tenantId)
+        {
+            string appId = string.Empty;
+            // TODO : Implement this method
+            return appId;
+        }
+
+        public List<string> GetUserExtensionAttributes(string userIdentifier, string tenantId)
+        {
+            List<string> extensionAttributes = new List<string>();
+            // TODO : Implement this method
+            return extensionAttributes;
+        }
+    }
+}
+
