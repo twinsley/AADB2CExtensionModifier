29d0a5f58aa7550ef539d5c3f8b73a068d934ee5
Timothy Winsley <t-winsley@protonmail.com>
2024-04-11 06:55:52 -0700

    Implement method to get B2C app, add logging.

--- AADB2CExtensionModifier/Services/GraphHandlerService.cs
+++ AADB2CExtensionModifier/Services/GraphHandlerService.cs
@@ -8,21 +8,34 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace AADB2CExtensionModifier.Services
 {
     internal class GraphHandlerService
     {
         // This method is used to get the B2C extension application id and parse it for use in querying the extension attributes.
-        public String GetB2cExtensionAppId(string tenantId, GraphServiceClient graphclient)
+        public String GetB2cExtensionAppId(GraphServiceClient graphclient)
         {
             string appId = string.Empty;
-            // TODO : Implement this method
+            ApplicationCollectionResponse apps = graphclient.Applications.GetAsync(requestConfig =>
+            {
+                requestConfig.QueryParameters.Select =
+                    ["id", "displayName", "appId"];
+                requestConfig.QueryParameters.Filter =
+                    $"startswith(displayname, 'b2c-extensions-app')";
+            }).Result;
+            Application app = apps.Value.FirstOrDefault();
+            if (app != null)
+            {
+                appId = app.AppId.Replace("-", "");
+            }
+            Console.WriteLine($"B2C Extension App Id: {appId}");
             return appId;
         }
 
         // This method gets the B2C extension attributes for the user.
-        public List<string> GetUserExtensionAttributes(string userIdentifier, string tenantId, GraphServiceClient graphclient)
+        public List<string> GetUserExtensionAttributes(string userIdentifier, GraphServiceClient graphclient)
         {
             List<string> extensionAttributes = new List<string>();
             // TODO : Implement this method
@@ -38,25 +51,29 @@ namespace AADB2CExtensionModifier.Services
         // This method gets the user's graph user object. It should take an email as input and return the user object.
         public User GetGraphUser(string email, GraphServiceClient graphclient)
         {
+            Console.WriteLine($"Getting user with email: {email}");
             UserCollectionResponse? users;
             users = graphclient.Users.GetAsync(requestConfig =>
             {
                 requestConfig.QueryParameters.Select =
-                    ["id", "fullname", "email"];
+                    ["id", "displayName", "mail"];
                 requestConfig.QueryParameters.Filter =
-                    $"subject eq '{email}'";
+                    $"mail eq '{email}'";
             }).Result;
             if (users == null)
             {
+                Console.WriteLine($"User with email {email} not found");
                 return null;
             }
             User user = users.Value.FirstOrDefault();
+            Console.WriteLine($"User found: {user.DisplayName}");
             return user;
         }
 
         // method to authenticate to graph api
         public GraphServiceClient GetGraphClient(string tenantId, string clientId, string[] scopes)
         {
+            Console.WriteLine("Creating graph client");
             var options = new InteractiveBrowserCredentialOptions
             {
                 TenantId = tenantId,
@@ -66,6 +83,7 @@ namespace AADB2CExtensionModifier.Services
             };
             var interactiveCredential = new InteractiveBrowserCredential(options);
             var graphClient = new GraphServiceClient(interactiveCredential, scopes);
+            Console.WriteLine("Graph client created");
             return graphClient;
         }
     }
